c67949eb86aee1c969b3de77eb6405158f0053b4
Stephen Brown <stephen.jr36@gmail.com>
2024-08-28 14:35:46 -0600

    added auth and logout routes

--- app.js
+++ app.js
@@ -24,4 +24,27 @@ app.get('/api/movies/:movieId', async (req, res) => {
   res.json(movie);
 });
 
+app.post('/api/auth', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    where: {email: email},
+  });
+
+  if (user && user.password === password) {
+    req.session.userId = user.userId;
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  };
+});
+
+app.post('/api/logout', async (req, res) => {
+  if (!req.session.userId) {
+    res.status(401).json({ error: 'Unauthorized' });
+  } else {
+    res.session.destroy();
+    res.json({ success: true })
+  }
+})
+
 ViteExpress.listen(app, port, () => console.log(`Server is listening on http://localhost:${port}`));
