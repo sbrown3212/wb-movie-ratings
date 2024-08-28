1e00da5269881fa4079b98ea94141675b73ccde6
Stephen Brown <stephen.jr36@gmail.com>
2024-08-28 15:50:20 -0600

    added comments to routes describing route purpose

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
 
+// Middleware to prevent access auth-required routes
 function loginRequired(req, res, next) {
   if (!req.session.userId) {
     res.status(401).json({ error: 'Unauthorized' });
@@ -21,58 +22,80 @@ function loginRequired(req, res, next) {
   };
 };
 
+// GET request that returns all movies in db
 app.get('/api/movies', async (req, res) => {
   const allMovies = await Movie.findAll();
   res.json(allMovies);
 });
 
+// GET request that returns an individual movie's data based on movieId param
 app.get('/api/movies/:movieId', async (req, res) => {
   const { movieId } = req.params;
   const movie = await Movie.findByPk(movieId);
   res.json(movie);
 });
 
+// POST request to log user in (save userId to session) if email exists in db and password matches user with corresponding email
 app.post('/api/auth', async (req, res) => {
   const { email, password } = req.body;
+
+  // Query db for user based on email provided in req.body
   const user = await User.findOne({
     where: {email: email},
   });
 
+  // If db returned user based on email, and password matches user's password
   if (user && user.password === password) {
+    // save userId to session and return success message
     req.session.userId = user.userId;
     res.json({ success: true });
   } else {
+    // otherwise, send failure message
     res.json({ success: false });
   };
 });
 
+// POST request to log user out if aready logged in (auth-required)
 app.post('/api/logout', loginRequired, (req, res) => {
+  // erase session data and send success message
   req.session.destroy();
   res.json({ success: true });
 });
 
+// GET request that returns all ratings created by user (auth-required)
 app.get('/api/ratings', loginRequired, async (req, res) => {
-  const user = await User.findByPk(1);
+  // Query db for user based on userId saved in session
+  const user = await User.findByPk(req.session.userId);
+
+  // Query db for ratings based on user and add movie title by joining Movie model 
   const ratings = await user.getRatings({
+    // Similar to JOIN in SQL (SELECT movie.title, JOIN movies)
     include: {
       model: Movie,
       attributes: ['title'],
     },
   });
+
+  // Responds with query results in JSON format
   res.json(ratings);
 });
 
+// POST request to add new rating (auth-required)
 app.post('/api/ratings', loginRequired, async (req, res) => {
   const { userId } = req.session;
 
   const { movieId, score } = req.body;
 
+  // Query db for user
   const user = await User.findByPk(userId);
+
+  // Create new rating in db based on user
   const rating = await user.createRating({
     movieId: movieId,
     score: score,
   });
-  
+
+  // Responds with newly created rating
   res.json(rating);
 });
 
