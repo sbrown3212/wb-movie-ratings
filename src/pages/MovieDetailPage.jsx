d31668f7ba4cdb8b15085f2786a0c5688f1039b5
Stephen Brown <stephen.jr36@gmail.com>
2024-08-29 14:37:55 -0600

    added handler function for user to submit a rating. Added CreateRatingForm component.

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,13 +1,33 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import CreateRatingForm from "../components/CreateRatingForm";
+import axios from "axios";
 
 export default function MovieDetailPage() {
-  const { movie } = useLoaderData();
+  const { movie: { title, posterPath, overview, movieId } } = useLoaderData();
+
+  const navigate = useNavigate();
+
+  // Creates handler function for CreateRatingForm to run when user clicks submit button
+  // Takes the event target and { score } object (which is provided by the form)
+  const handleCreateRating = async (e, { score }) => {
+    e.preventDefault();
+
+    // Axios POST request to add a rating by user based on the current page's movie movieId
+    const res = await axios.post('/api/ratings', { score: score, movieId: movieId });
+
+    // If the post was successful, navigate to '/me' (Your Ratings page)
+    if (res.data) {
+      navigate('/me');
+    }
+  }
 
   return (
     <>
-      <h1>{movie.title}</h1>
-      <img src={movie.posterPath} alt="" style={{ width: '200px'}} />
-      <p>{movie.overview}</p>
+      <h1>{title}</h1>
+      <img src={posterPath} alt="" style={{ width: '200px'}} />
+      <p>{overview}</p>
+      <h2>Rate this movie</h2>
+      <CreateRatingForm onCreateRating={handleCreateRating} /> {/* Passes function as prop for form component to run with submit event */}
     </>
   );
 }
