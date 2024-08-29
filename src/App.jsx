82f5e82c09c58385dc1c63d1b0f4c7532688b30c
Stephen Brown <stephen.jr36@gmail.com>
2024-08-29 14:02:26 -0600

    added handleLogout function to log out user and navigate to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import LogoutButton from './components/LogoutButton.jsx';
+import axios from 'axios';
 
 export default function App() {
+  const navigate = useNavigate();
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.post('/api/logout', );
+    if (res.data.success) {
+      navigate('/');
+    }
+  }
+
   return (
     <>
       <nav>
@@ -19,7 +31,7 @@ export default function App() {
             <NavLink to="/me">Your ratings</NavLink>
           </li>
           <li>
-            <LogoutButton />
+            <LogoutButton onLogout={handleLogout}/>
           </li>
         </ul>
       </nav>
