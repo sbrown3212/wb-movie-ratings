c5dc113bb66a6cca75ebec21c9c1a8404774a1d1
Stephen Brown <stephen.jr36@gmail.com>
2024-08-29 12:36:56 -0600

    added handleLogin function to log use in LoginForm onLogin prop

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,24 @@
+import LoginForm from "../components/LoginForm";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 export default function LoginPage() {
+  const navigate = useNavigate();
+
+  const handleLogin = async (e, formData) => {
+    e.preventDefault();
+
+    const res = await axios.post('/api/auth', formData);
+
+    if (res.data.success) {
+      navigate('/me');
+    }
+  };
+
   return (
     <>
       <h1>Log In</h1>
-      <p>TODO</p>
+      <LoginForm onLogin={handleLogin} />
     </>
   );
 }
