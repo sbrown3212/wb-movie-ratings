e3e5bd94c4df5c5f296c810bbee99e8fe65c487a
Stephen Brown <stephen.jr36@gmail.com>
2024-08-29 11:20:18 -0600

    added react route for '/movies' on All movies link

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from 'react-router-dom';
-import App from './App.jsx';
-import './css/index.css';
-import ErrorPage from './pages/ErrorPage.jsx';
-import IndexPage from './pages/IndexPage.jsx';
+} from "react-router-dom";
+import App from "./App.jsx";
+import "./css/index.css";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import IndexPage from "./pages/IndexPage.jsx";
+import AllMoviesPage from "./pages/AllMoviesPage.jsx";
+import axios from "axios";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} errorElement={<ErrorPage />}>
       <Route index element={<IndexPage />} />
-    </Route>,
-  ),
+      <Route 
+        path="/movies"
+        element={<AllMoviesPage />}
+        loader={async () => {
+          const res = await axios.get('/api/movies');
+          return {movies: res.data};
+        }}
+      />
+    </Route>
+  )
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
