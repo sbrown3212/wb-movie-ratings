dd2ff01ccafd0d34bb7ad9865716fcfff9ec43c6
Stephen Brown <stephen.jr36@gmail.com>
2024-08-29 12:39:29 -0600

    added comments for clarification

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,11 +5,15 @@ import axios from "axios";
 export default function LoginPage() {
   const navigate = useNavigate();
 
+  // Logs user in if credentials are correct
   const handleLogin = async (e, formData) => {
+    // Prevents page from reloading on submit
     e.preventDefault();
 
+    // Axios POST request to log user in
     const res = await axios.post('/api/auth', formData);
 
+    // If login was successful, navigate to '/me' route (YourRatings page)
     if (res.data.success) {
       navigate('/me');
     }
